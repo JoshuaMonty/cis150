01564f7f91c6d147a671f52aeb1fa519f1c4a9b1
Joshua_Monty <jsoldierlake@gmail.com>
2017-03-30 08:05:43 -0700

    Added Process Commands Changed case conditions Added subfunctions

--- project5/main.sh
+++ project5/main.sh
@@ -1,27 +1,71 @@
 #!/bin/bash
 PS3='Please make a selection:'
 main() {
-	options=("Editing Commands" "Games" “Chess” “Checkers” "User Administration" "Process Commands" "System Commands" "Network Commands")
+	tput reset
+	options=("Editing Commands" "Games" "User Administration" "Process Commands" "System Commands" "Network Commands" "Exit")
 	select opt in "${options[@]}"
 	do
     	case $opt in
-        "Editing Commands")
-            echo "edit FILENAME"
-            ;;
-        "Games")
-            echo "Chess or Checkers?"
-            ;;
-        "User Administration")
-            ;;
-        "Process Commands")
-            echo "ps -a, kill"
-            ;;
-        "System Commands")
-            ;;
-        "Network Commands")
-            ;;
+        "${options[0]}")
+		editingcmds
+            	;;
+        "${options[1]}")
+		games
+            	;;
+        "${options[2]}")
+            	;;
+        "${options[3]}")
+            	processcmds
+            	;;
+        "${options[4]}")
+		;;
+        "${options[5]}")
+		;;
+	"${options[6]}")
+		exit 0
+		;;
         *) echo invalid option;;
     	esac
 	done
+}
+
+editingcmds() {
+	echo "edit FILENAME"
+
+}
+games() {
+	echo "Chess or Checkers?"
+
+}
+useradmin() {
+	echo ""
+}
+processcmds() {
+	options=("List User Prox" "List All Prox" "Kill Prox" "Return to Main")
+	select opt in "${options[@]}"
+	do
+	case $opt in
+	"${options[0]}")
+		ps ux | more
+		processcmds
+		;;
+	"${options[1]}")
+		ps aux | more
+		processcmds
+		;;
+	"${options[2]}")
+		;;
+	"${options[3]}")
+		main
+		;;
+	*) echo invalid option;;
+	esac
+	done
+}
+systemcommands() {
+	echo ""
+}
+networkcommands() {
+	echo ""
 }
 main
