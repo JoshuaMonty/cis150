946a29146e794f242ee8dfe12cdf205f7b2cf703
Joshua_Monty <jsoldierlake@gmail.com>
2017-04-10 11:38:24 -0700

    Added writeout and removed unused functions

--- project6/main.sh
+++ project6/main.sh
@@ -1,54 +1,12 @@
+# Usage: ./main.sh 65 <employees.csv
 IFS=","
 
-declare inFile="$1"
-declare outFile="$2"
-declare -a lastNames
-declare -a firstNames
-declare -a ages
-declare -a people
+declare -A people
 
 main() {
-   # readFile
-   # altReadFile
     readin
-   # printLastNames
-   # printMdArray
-   # writeNewFile
+    writeout $1
     exit 0
-}
-
-readFile() {
-    declare -i lineNumber=0
-    while read lastName firstName age
-    do
-        lastNames[$lineNumber]=$lastName
-        firstNames[$lineNumber]=$firstName
-        ages[$lineNumber]=$age
-        (( lineNumber++ ))
-    done < $inFile
-}
-
-altReadFile() {
-	declare -i lineNumber=0
-	declare -ir ROWS=6
-	declare -ir COLUMNS=3
-	while read lastName firstName age
-	do
-		for (( i=0; i < ROWS; i++ )) {
-			for (( j=0; j < COLUMNS; j++ )) {
-				if [ $j -eq 0 ]
-				then
-					people[$i,$j]=$lastName
-				elif [ $j -eq 1 ]
-				then
-					people[$i,$j]=$firstName
-				else
-					people[$i,$j]=$age
-				fi
-			}
-		}
-		(( lineNumber++ ))
-	done < $inFile
 }
 
 readin() {
@@ -57,35 +15,22 @@ readin() {
 	do
 		people[$i,0]=$lastName
 		people[$i,1]=$firstName
-		people[$i,2]=$age
-	echo "$i $lastName $firstName $age"
+		people[$i,2]=$age	
 		(( i++ ))
 	done
 }
-	
-printLastNames() {
-    for (( i=0; i < ${#lastNames[@]}; i++ ))
-    do
-        echo -e "${lastNames[i]}\n"
-    done
+
+writeout() {
+	declare -i i=0
+	while [ ${people[$i,0]+isset} ]
+	do
+		if [ $(expr ${people[$i,2]} - $1) -gt 0 ]; then
+
+			echo "${people[$i,0]}"
+		fi
+		(( i++ ))
+	done
 }
 
-printMdArray() {
-	declare -ir ROWS=6
-	declare -ir COLUMNS=3
-	for (( i=0; i < ROWS; i++ )) {
-		for (( j=0; j < COLUMNS; j++ )) {
-			echo "${people[$i,$j]}"
-		}
-	}
-}
-
-writeNewFile() {
-     for (( i=0; i < ${#lastNames[@]}; i++ ))
-     do
-         echo ${lastNames[i]} >> $outFile
-     done
-}
-
-main
+main $1
 
