c72420e4c14f8306d00f3a5e379382f31b4ed058
Joshua_Monty <jsoldierlake@gmail.com>
2017-05-04 00:14:27 -0400

    Prints user once finshed

--- project8/main.pl
+++ project8/main.pl
@@ -18,10 +18,21 @@ main();
 sub parseargs {
 	$username = trim ($ARGV[0]?$ARGV[0]:`whoami`);
 	$zipfile = $ARGV[1]?$ARGV[1]:"$username.tar.gz";
-	$path = $ARGV[2]?$ARGV[2]:"./";
-	#print "$username, $zipfile, $path";
+	$path = $ARGV[2]?$ARGV[2]:findpath();
+	print "$username, $zipfile, $path";
 }
-
+sub findpath {
+	my $passwd = trim (`grep "^$username" /etc/passwd`);
+	my $hash = {
+		username => 0,
+		home => 5
+	};
+	if (!$passwd) {
+		return "./";
+	}
+	$passwd = split (/:/,$passwd);
+	return [$hash->{'home'}];
+}
 sub findfiles {
 	$files = `find $path -user $username`;
 	print "Archiving:\n$files\n";
